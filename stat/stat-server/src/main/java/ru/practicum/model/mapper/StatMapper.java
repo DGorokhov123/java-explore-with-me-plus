dfb0caac19ca15ee931c2279a6394b7d2cc9971a
Ivan G <grikoivan@yandex.ru>
2025-07-08 23:10:09 +0300

    Add stat_svc_service

--- stat/stat-server/src/main/java/ru/practicum/model/mapper/StatMapper.java
+++ stat/stat-server/src/main/java/ru/practicum/model/mapper/StatMapper.java
@@ -1,30 +1,25 @@
 package ru.practicum.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 import ru.practicum.EventHitDto;
 import ru.practicum.model.Stat;
 
-public class StatMapper {
+@Mapper
+public interface StatMapper {
 
-    private StatMapper() {
-    }
+    StatMapper INSTANCE = Mappers.getMapper(StatMapper.class);
 
-    public static Stat toStat(EventHitDto statDto) {
+    @Mapping(source = "app", target = "app")
+    @Mapping(source = "uri", target = "uri")
+    @Mapping(source = "ip", target = "ip")
+    @Mapping(source = "timestamp", target = "timestamp")
+    Stat toStat(EventHitDto statDto);
 
-        return Stat.builder()
-                .app(statDto.getApp())
-                .uri(statDto.getUri())
-                .ip(statDto.getIp())
-                .timestamp(statDto.getTimestamp())
-                .build();
-    }
-
-    public static EventHitDto toEventHitDto(Stat stat) {
-        return EventHitDto.builder()
-                .app(stat.getApp())
-                .uri(stat.getUri())
-                .ip(stat.getIp())
-                .timestamp(stat.getTimestamp())
-                .build();
-    }
-
+    @Mapping(source = "app", target = "app")
+    @Mapping(source = "uri", target = "uri")
+    @Mapping(source = "ip", target = "ip")
+    @Mapping(source = "timestamp", target = "timestamp")
+    EventHitDto toEventHitDto(Stat stat);
 }
