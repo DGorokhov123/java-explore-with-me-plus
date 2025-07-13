aacc77b353940eff99b8e21953a529064e1fde75
Ivan G <grikoivan@yandex.ru>
2025-07-13 16:25:03 +0300

    Добавлены таблицы подборок в схему БД, ДТО, репозиторий и валидатор операций

---
+++ server/src/main/java/ru/practicum/compilation/CompilationDtoNew.java
@@ -0,0 +1,26 @@
+package ru.practicum.compilation;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import ru.practicum.validation.CreateOrUpdateValidator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CompilationDtoNew {
+
+    Set<Long> events = new HashSet<>();
+
+    Boolean pinned;
+
+    @NotBlank(groups = {CreateOrUpdateValidator.Create.class})
+    @Size(max = 50, groups = {CreateOrUpdateValidator.Create.class, CreateOrUpdateValidator.Update.class})
+    String title;
+}
